55114eba8bb5638574f9515c6c0b79b6a501ba89
VitaliiZvieriev <zverew91@gmail.com>
2025-04-05 13:44:15 +0300

    add thunk and select

---
+++ src/components/app/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContacts } from "../../redux/contactsOps";
+import { PropagateLoader } from "react-spinners";
+import ContactForm from "../contactForm/ContactForm";
+import SearchBox from "../searchBox/SearchBox";
+import ContactList from "../contactList/ContactList";
+// It's project was created by Vitalii Zvieriev
+import "./App.css";
+import {
+  selectContact,
+  selectErorr,
+  selectLoading
+} from "../../redux/contactsSlice";
+
+function App() {
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectErorr);
+  const contacts = useSelector(selectContact);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <p>
+        Project created by{" "}
+        <a href="https://github.com/Vitaliy-Z" target="_black">
+          Vitalii Zvieriev
+        </a>
+      </p>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <hr />
+      <SearchBox />
+      {loading && <PropagateLoader color="red" size={17} />}
+      {error && <p>Error...</p>}
+      {contacts.length > 0 && <ContactList />}
+    </>
+  );
+}
+
+export default App;
